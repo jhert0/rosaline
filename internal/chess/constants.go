c68922b39031e1db464a47f5b543a8842b1d5f6f
Jeremy Hertel <dev@jhert.com>
2022-11-26 13:17:20 -0600

    chess: change value of castling rights constants

--- internal/chess/constants.go
+++ internal/chess/constants.go
@@ -7,12 +7,12 @@ const (
 type CastlingRights uint8
 
 const (
-	WhiteCastleKingside  CastlingRights = 0b0001
-	WhiteCastleQueenside CastlingRights = 0b0010
+	WhiteCastleKingside  CastlingRights = 0b0100
+	WhiteCastleQueenside CastlingRights = 0b1000
 	WhiteCastleBoth      CastlingRights = WhiteCastleKingside | WhiteCastleQueenside
 
-	BlackCastleKingside  CastlingRights = 0b0100
-	BlackCastleQueenside CastlingRights = 0b1000
+	BlackCastleKingside  CastlingRights = 0b0001
+	BlackCastleQueenside CastlingRights = 0b0010
 	BlackCastleBoth      CastlingRights = BlackCastleKingside | BlackCastleQueenside
 )
 
