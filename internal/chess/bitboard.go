1e629afbe1555503caa62479c75e944775b95e46
Jeremy Hertel <dev@jhert.com>
2023-10-24 17:42:34 -0500

    chess: add documentation to BitBoard functions

--- internal/chess/bitboard.go
+++ internal/chess/bitboard.go
@@ -7,10 +7,12 @@ import (
 
 type BitBoard uint64
 
+// NewBitBoard creates a new BitBoard with the give value.
 func NewBitBoard(value uint64) BitBoard {
 	return BitBoard(value)
 }
 
+// SetBit sets the bit at the given bit number.
 func (bb *BitBoard) SetBit(number uint64) {
 	if number > 63 {
 		panic(fmt.Sprintf("tried to set invalid bit number: %d", number))
@@ -19,6 +21,7 @@ func (bb *BitBoard) SetBit(number uint64) {
 	*bb |= BitBoard(uint64(1) << number)
 }
 
+// ClearBit clears the bit at the given bit number.
 func (bb *BitBoard) ClearBit(number uint64) {
 	if number > 63 {
 		panic(fmt.Sprintf("tried to clear invalid bit number: %d", number))
@@ -27,6 +30,7 @@ func (bb *BitBoard) ClearBit(number uint64) {
 	*bb &= BitBoard(^(uint64(1) << number))
 }
 
+// IsBitSet checks if the bit is set at the given bit number.
 func (bb BitBoard) IsBitSet(number uint64) bool {
 	if number > 63 {
 		panic(fmt.Sprintf("tried to check value of invalid bit number: %d", number))
@@ -35,14 +39,17 @@ func (bb BitBoard) IsBitSet(number uint64) bool {
 	return (bb & BitBoard((uint64(1) << number))) > 0
 }
 
+// TrailingZeros returns the number of trailing zeros.
 func (bb BitBoard) TrailingZeros() int {
 	return bits.TrailingZeros64(uint64(bb))
 }
 
+// PopulationCount returns the number of bits that are set.
 func (bb BitBoard) PopulationCount() int {
 	return bits.OnesCount64(uint64(bb))
 }
 
+// Print prints the BitBoard value in binary.
 func (bb BitBoard) Print() {
 	fmt.Printf("%064b\n", bb)
 }
