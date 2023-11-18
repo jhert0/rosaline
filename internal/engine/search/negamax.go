f68cd086eae0f7cb0f6092be0abf5a3377f40421
Jeremy Hertel <dev@jhert.com>
2023-11-17 21:21:25 -0600

    engine: fix incorrect search implementation

--- internal/engine/search/negamax.go
+++ internal/engine/search/negamax.go
@@ -36,22 +36,6 @@ func (s negamaxSearcher) Search(position chess.Position, depth int) ScoredMove {
 	return bestMove
 }
 
-func (s negamaxSearcher) adjustScore(turn chess.Color, score int) int {
-	if turn == chess.White {
-		if score < 0 {
-			return -score
-		}
-
-		return score
-	} else {
-		if score > 0 {
-			return -score
-		}
-
-		return score
-	}
-}
-
 func (s *negamaxSearcher) doSearch(position chess.Position, depth int) int {
 	if depth == 0 {
 		return s.evaluator.Evaluate(position)
@@ -68,15 +52,8 @@ func (s *negamaxSearcher) doSearch(position chess.Position, depth int) int {
 		position.MakeMove(move)
 
 		score := -s.doSearch(position, depth-1)
-
-		// for negamax the score needs to be from the perspective of the
-		// current players turn but the evaluator will return who is winning
-		// regardless of who's turn it is,thus we need to adjust the score if the
-		// opponent is winning
-		adjusted := s.adjustScore(position.Turn(), score)
-
-		if adjusted > max {
-			max = adjusted
+		if score > max {
+			max = score
 		}
 
 		position.Undo()
