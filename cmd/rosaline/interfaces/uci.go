16eb6e8dca3c86bb838758b647f6c3a3a112b324
Jeremy Hertel <dev@jhert.com>
2023-11-18 12:12:33 -0600

    rosaline: implement basic uci support

--- cmd/rosaline/interfaces/uci.go
+++ cmd/rosaline/interfaces/uci.go
@@ -1,5 +1,14 @@
 package interfaces
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"rosaline/internal/chess"
+	"rosaline/internal/engine"
+	"rosaline/internal/utils"
+)
+
 type uciInterface struct {
 }
 
@@ -8,5 +17,37 @@ func NewUciProtocolHandler() uciInterface {
 }
 
 func (i uciInterface) Loop() {
+	engine := engine.NewEngine()
 
+	scanner := bufio.NewScanner(os.Stdin)
+
+loop:
+	for {
+		scanner.Scan()
+		cmd, args := utils.ParseCommand(scanner.Text())
+
+		switch cmd {
+		case "uci":
+			fmt.Println("id name rosaline")
+			fmt.Println("id author rosaline contributors")
+			fmt.Println("uciok")
+			break
+		case "isready":
+			fmt.Println("readyok")
+			break
+		case "ucinewgame":
+			engine.NewGame(chess.StartingFen)
+			break
+		case "position":
+			lastMove := args[len(args)-1]
+			engine.Game.MakeUciMove(lastMove)
+			break
+		case "go":
+			move := engine.PlayBestMove()
+			fmt.Println("bestmove", move)
+			break
+		case "quit":
+			break loop
+		}
+	}
 }
