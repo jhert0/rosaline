ba3763ce03dbed38040110467549ca2443c90e9b
Jeremy Hertel <dev@jhert.com>
2023-11-19 23:09:18 -0600

    evaluation: improve comments

--- internal/engine/evaluation/evaluator.go
+++ internal/engine/evaluation/evaluator.go
@@ -11,7 +11,10 @@ func NewEvaluator() Evaluator {
 	return Evaluator{}
 }
 
-// evaluateSide determines a score for a side based on the value of the pieces it has.
+// evaluateSide determines the score for a side based:
+//   - The value of it's pieces.
+//   - The location of it's pieces.
+//   - Bonuses/penalties for advantageous/disadvantageous positions.
 func (e Evaluator) evaluateSide(position chess.Position, color chess.Color) int {
 	score := 0
 
@@ -19,7 +22,7 @@ func (e Evaluator) evaluateSide(position chess.Position, color chess.Color) int 
 	for colorBB > 0 {
 		square := chess.Square(colorBB.PopLsb())
 		piece, _ := position.GetPieceAt(square)
-		score += pieceValue(piece)
+		score += PieceValue(piece)
 		score += e.getSquareScore(position, piece, square)
 	}
 
@@ -27,6 +30,8 @@ func (e Evaluator) evaluateSide(position chess.Position, color chess.Color) int 
 	ourPawns := colorBB & pawnBB
 	for _, bb := range chess.FileBitBoards {
 		pawns := ourPawns & bb
+
+		// give a penalty for two or more pawns of the same color being in a file
 		if pawns.PopulationCount() >= 2 {
 			score += doublePawnPenalty
 		} else if pawns.PopulationCount() == 0 {
@@ -34,11 +39,12 @@ func (e Evaluator) evaluateSide(position chess.Position, color chess.Color) int 
 		}
 
 		pawns = pawnBB & bb
-		if pawns.PopulationCount() == 0 {
+		if pawns.PopulationCount() == 0 { // add a bonus for no pawns being in a file
 			score += openFileBonus
 		}
 	}
 
+	// add bonus for having 2 or more bishops
 	bishopBB := colorBB & position.GetPieceBB(chess.Bishop)
 	if bishopBB.PopulationCount() >= 2 {
 		score += doubleBishopBonus
@@ -47,6 +53,7 @@ func (e Evaluator) evaluateSide(position chess.Position, color chess.Color) int 
 	return score
 }
 
+// getSquareScore returns the score for the given piece being on the give square.
 func (e Evaluator) getSquareScore(position chess.Position, piece chess.Piece, square chess.Square) int {
 	if position.Phase() == chess.EndgamePhase {
 		scoreBoard, ok := openingScores[piece]
@@ -63,6 +70,12 @@ func (e Evaluator) getSquareScore(position chess.Position, piece chess.Piece, sq
 	return 0
 }
 
+// Evaluate returns the score of the current position.
+//
+// The score will be:
+//   - positive: if white is winning
+//   - zero: if the position is a draw
+//   - negative: if black is winning
 func (e Evaluator) Evaluate(position chess.Position) int {
 	if position.IsDraw() {
 		return DrawScore
